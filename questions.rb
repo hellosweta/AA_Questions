9f9c931756ff35d5e4613af9bf479c1368772eba
Brodrick Childs <brodrick.childs@gmail.com>
2017-02-07 11:33:01 -0800

    Create model class for each table

---
+++ questions.rb
@@ -0,0 +1,132 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class User
+  attr_accessor :fname, :lname
+
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    return nil if user.empty?
+
+    User.new(user.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end
+
+class Question
+  attr_accessor :title, :body, :associated_author_id
+
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+    return nil if question.empty?
+    Question.new(question.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @associated_author_id = options['associated_author_id']
+  end
+end
+
+class QuestionFollow
+  attr_accessor :question_id, :user_id
+
+  def self.find_by_id(id)
+    question_follow = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+    return nil if question_follow.empty?
+    QuestionFollow.new(question_follow.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
+
+class Reply
+  attr_accessor :subject_question_id, :parent_reply_id, :user_id, :body
+
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+    return nil if reply.empty?
+    Reply.new(reply.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @subject_question_id = options['subject_question_id']
+    @parent_reply_id = options['parent_reply_id']
+    @user_id = options['user_id']
+    @body = options['body']
+  end
+end
+
+class QuestionLike
+  attr_accessor :user_id, :question_id
+
+  def self.find_by_id(id)
+    question_like = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_likes
+      WHERE
+        id = ?
+    SQL
+    return nil if question_like.empty?
+    QuestionLike.new(question_like.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+end
